2cd5b0f411383f2af4190410abda203b35039a11
margaerhy <kimrlyunah@gmail.com>
2021-11-10 10:18:36 +0900

    feat - authorization edit

--- src/main/java/projectBlog/customBlog/interceptor/AuthorizationInterceptor.java
+++ src/main/java/projectBlog/customBlog/interceptor/AuthorizationInterceptor.java
@@ -1,5 +1,6 @@
 package projectBlog.customBlog.interceptor;
 
+import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -24,12 +25,26 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
 
         Integer blogId = (Integer) model.getAttribute("blogId");
 
-        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
-
-        /*if(blogId != member.getBlog().getId()) {
-            log.info("블로그 주인이 아닌 로그인 사용자의 요청");
-            response.sendRedirect("/?redirectURL=" + requestURI);
+        if(session == null) {
+            log.info("비회원 요청");
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+            return;
         }
-        */
+
+        Enumeration enumSession = session.getAttributeNames();
+
+        while(enumSession.hasMoreElements()) {
+            String se = enumSession.nextElement() +"";
+            Member member = (Member) session.getAttribute(se);
+            if(member != null) {
+                if(blogId != member.getBlog().getId()) {
+                    log.info("블로그 주인이 아닌 로그인 사용자의 요청");
+                    response.sendRedirect("/login?redirectURL=" + requestURI);
+                }
+            } else {
+                log.info("비회원 요청");
+                response.sendRedirect("/login?redirectURL=" + requestURI);
+            }
+        }
     }
 }
